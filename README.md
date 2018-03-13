3d371e284ed4dbffbc158fa5b3a828c3aa432f03
Fernando Fabricio dos Santos <ffsantos@uolinc.com>
2018-03-13 16:10:26 -0300

    Iniciando

---
+++ README.md
@@ -0,0 +1,11 @@
+# Criando API GraphQL
+
+## Hello world
+
+`npm install --save express-graphql graphql`
+
+## Criando um modelo no banco
+
+## Criando um mutation
+
+## Criando mais de um type
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => 'Hello World!' };
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(3000, () => console.log('Executando na porta 3000'));
